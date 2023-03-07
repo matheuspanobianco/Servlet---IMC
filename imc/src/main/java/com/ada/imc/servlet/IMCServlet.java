b80124d5d661dff1ea4a939e8878f3242489d224
Matheus Panobianco <matheuspanobianco@hotmail.com>
2023-03-07 18:49:13 -0300

    servlet_imc

---
+++ imc/src/main/java/com/ada/imc/servlet/IMCServlet.java
@@ -0,0 +1,23 @@
+package com.ada.imc.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "IMCServlet", urlPatterns = "/calc")
+public class IMCServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Double peso = Double.valueOf((req.getParameter("peso")));
+        Double altura = Double.valueOf((req.getParameter("altura")));
+        Double imc = peso / (altura * altura);
+        req.setAttribute("imc", String.format("%.2f %%", imc));
+        resp.setHeader("imc", String.format("%.2f", imc));
+        req.getRequestDispatcher("/index.jsp").forward(req, resp);
+    }
+
+}
